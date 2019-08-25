afed238e016fa712efcdeaac2382b3a29ad0c10e
vrushabh deshmukh <itsvrushabh@gmail.com>
2019-08-25 09:40:34 +0530

    added the .sh file to config the rabbitmq

---
+++ cluster-entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+# Start RMQ from entry point.
+# This will ensure that environment variables passed
+# will be honored
+/usr/local/bin/docker-entrypoint.sh rabbitmq-server -detached
+
+# Do the cluster dance
+rabbitmqctl stop_app
+rabbitmqctl join_cluster --disc rabbit@rabbitmq1
+
+#this command requires the 'rabbit' app to be running on the target node.
+#Start it with 'rabbitmqctl start_app'.
+rabbitmqctl start_app
+
+#Ex: Policy where queues whose names begin with "ha." are mirrored to all nodes 
+#in the cluster: $ rabbitmqctl set_policy ha-all "^ha\." '{"ha-mode":"all"}'
+rabbitmqctl set_policy ha-all "" '{"ha-mode":"all","ha-sync-mode":"automatic"}'
+
+# Stop the entire RMQ server. This is done so that we
+# can attach to it again, but without the -detached flag
+# making it run in the forground
+rabbitmqctl stop
+
+# Wait a while for the app to really stop
+sleep 2s
+
+# Start it
+rabbitmq-server
